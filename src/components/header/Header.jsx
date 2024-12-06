4d40ce0006924c6de4b075b0fe9d14d089e958d5
MANIVANNAN777 <manismartsoftware@gmail.com>
2024-12-06 18:52:18 +0530

    first commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,10 +7,10 @@ const Header = () => {
   return (
     <header id="home">
       <div className="container header__container">
-        <h1>Mr.J.Manivannan M.E.,</h1>
-        <h3 className="text-light">
+        <p>Mr.J.Manivannan M.E.,</p>
+        <h4 className="text-light">
           Assistant Professor & Front-End-Developer
-        </h3>
+        </h4>
         <CTA />
         <a href="#contact" className="scroll__down">
           Scroll Down
